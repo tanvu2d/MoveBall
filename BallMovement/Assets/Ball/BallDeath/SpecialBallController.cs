be2dc4b76bdde066f70b0042b9f38651c9921466
tanvu <tanvu@DESKTOP-9SAQ0GS>
2022-03-06 01:29:06 +0700

    add ball death

---
+++ BallMovement/Assets/Ball/BallDeath/SpecialBallController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialBallController : BallController
+{
+
+     void OnMouseDown ()
+    {
+        Debug.Log("done");
+    }
+}
